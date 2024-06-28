d7f85b8b9bfe0bc47577e0a1860ae055dc8b1255
BhaskarDeb2000 <bhaskardeb120@gmail.com>
2024-06-28 13:39:49 +0300

    add products page :zap:

--- routes/addProduct.js
+++ routes/addProduct.js
@@ -5,7 +5,6 @@ const bodyParser = require("body-parser");
 const rootDir = require("../helpers/path");
 
 const addProduct = express.Router();
-const products = [];
 
 addProduct.use(bodyParser.urlencoded({ extended: true }));
 
@@ -14,10 +13,6 @@ addProduct.get(`/add-product`, (req, res, next) => {
 });
 
 addProduct.post("/added-products", (req, res, next) => {
-  products.push({ Title: req.body.productName });
   res.redirect("/");
 });
-// module.exports = adminRoute;
-
-exports.addProduct = addProduct;
-exports.products = products;
+module.exports = addProduct;
